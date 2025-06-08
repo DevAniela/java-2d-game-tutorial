6d418b5ad7eb67e4aa116ce44a9478d6a9d3dce4
Ana Vi <dismal.damsel@gmail.com>
2025-06-08 13:03:06 +0300

    Make small map with 3 types of tiles (grass, wall, and water) - added 3 png tile types (grass, wall, water) - introduced background tiles automated draw - create Tile and TileManager class - create map data and save to text file - import map data - draw map on screen

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,6 +3,7 @@ package main;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -16,22 +17,18 @@ public class GamePanel extends JPanel implements Runnable {
 	final int scale = 3;
 
 	public final int tileSize = originalTileSize * scale; // 48x48 tile
-	final int maxScreenCol = 16;
-	final int maxScreenRow = 12;
-	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+	public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
 	// FPS
 	int FPS = 60;
 
+	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	Player player = new Player(this, keyH);
-
-	// Set player's default position
-	int playerX = 100;
-	int playerY = 100;
-	int playerSpeed = 4;
 
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -123,6 +120,8 @@ public class GamePanel extends JPanel implements Runnable {
 
 		Graphics2D g2 = (Graphics2D) g;
 
+		tileM.draw(g2);
+		
 		player.draw(g2);
 
 		g2.dispose();
